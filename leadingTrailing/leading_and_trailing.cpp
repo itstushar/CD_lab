4e2d55c30454472c4be59e5a3226f1a6db037760
Tushar Garg <72547431+itstushar@users.noreply.github.com>
2021-01-04 22:16:45 +0530

    Update leading_and_trailing.cpp

--- leadingTrailing/leading_and_trailing.cpp
+++ leadingTrailing/leading_and_trailing.cpp
@@ -55,8 +55,6 @@ void installt(int a, int b)
 
 int main()
 {
-    cout << "Name : Yogesh Gaur " << endl;
-    cout << "Roll No : 1816110248" << endl;
     int i, s, k, j, n;
     char pr[30][30], b, c;
     cout << "Enter the no of productions:";
